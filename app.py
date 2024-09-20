e6c103a2a01f24f36b060aeef5a2e4279312bfc7
JuHye Mun <juhyemun@gmail.com>
2024-09-20 15:20:40 -0700

    got the detailed album info from album id, like popularity

--- app.py
+++ app.py
@@ -5,6 +5,9 @@
 # output the average length (calculate)
 # what other data do i want?
     # release date, number of tracks, genres, popularity
+    # it seems like genres often are empty
+    # perhaps, later, getting the copyright or label info (how many albums i liked were from major record labels)
+# work on getting genres and popularity
 
 # 3 
 # individual tracks' audio features (dancebility etc)
@@ -44,10 +47,16 @@ def get_album_info(album_title, artist_name):
     query = f'album:{album_title} artist:{artist_name}' # spotify api syntax for this search query
     result = sp.search(q=query, type='album', limit=1) # returns the album, 1
     
+    # print(result)
     if result['albums']['items']:
         # parse JSON to get the first (and only) album for its info
         album = result['albums']['items'][0]
         album_id = album['id']
+
+        # detailed info like genre and popularity
+        album_details = sp.album(album['id'])
+        print(album_details['popularity'])
+        # print(album_details['genres'])
 
         # Get album tracks
         tracks = sp.album_tracks(album_id)
@@ -64,6 +73,8 @@ def get_album_info(album_title, artist_name):
             'release_date': album['release_date'],
             'total_tracks': album['total_tracks'],
             'total_duration_min': total_duration_min,
+            # 'genres': album.get('genres', []), #avoid error if no genres specified
+            'popularity': album_details['popularity'],
             'uri': album['uri']
         }
     else:
@@ -86,6 +97,7 @@ def main():
         artist_name = row['artist_name']
 
         album_data = get_album_info(album_title, artist_name)
+        # print(album_data)
 
         if album_data:
             album_data_list.append(album_data)
@@ -97,11 +109,17 @@ def main():
     # print(album_data_df)
     # output the average length (calculate)
         # Calculate average album length
-    if not album_data_df.empty:
-        average_length = album_data_df['total_duration_min'].mean()
-        print(f'Average album duration: {average_length:.2f} minutes') # add a MM:SS function later
-    else:
-        print('No album data available.')
+    # if not album_data_df.empty:
+    #     average_length = album_data_df['total_duration_min'].mean()
+    #     print(f'Average album duration: {average_length:.2f} minutes') # add a MM:SS function later
+    # else:
+    #     print('No album data available.')
+
+    # access genre by album name
+    # album_name = 'GUTS'
+    # first_album_genre = album_data_df[album_data_df['album_name'] == album_name]['genres'].values[0]
+    # print(first_album_genre)
+
 
 if __name__ == "__main__":
     main()
